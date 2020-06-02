eeccc22d50e150925af130f61892a07661c90b82
khoadangatd <66126254+khoadangatd@users.noreply.github.com>
2020-06-02 19:25:57 +0700

    slides

---
+++ index.js
@@ -0,0 +1,11 @@
+var Btnright=document.querySelector(".header__slides__list__btn--right");
+var Btnleft=document.querySelector(".header__slides__list__btn--left");
+var x=0;
+function turnLeft(e){
+    var slides=document.querySelectorAll(".header__slides__list");
+    for(var i=0;i<slides.length;i++)
+    {
+        slides[i]
+    }
+}
+BtnLeft.addEventListener("click",function())
